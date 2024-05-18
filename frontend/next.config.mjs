21b6cb09ce2cc44a1ee8811dec2edb1fa0b1cf14
alkshmir <shirakam.itsuk252@gmail.com>
2024-05-18 23:26:05 +0900

    frontend: change config to ES module

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
-
-module.exports = {
+const nextConfig = {
+    // 他の設定オプションがあればここに追加
     async rewrites() {
         return [
             {
-                source: '/api/:path',
-                destination: 'http://host.docker.internal:8000/api/:path',
+                source: '/api/:path*',
+                destination: 'http://host.docker.internal:8000/api/:path*/',
             }
-        ]
+        ];
     }
-}
+};
+
+export default nextConfig;
